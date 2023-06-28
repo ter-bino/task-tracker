d728b93665ed2673bdda588093fcc201e36f692e
Teral <altero31@gmail.com>
2023-06-29 00:44:52 +0800

    some style change

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -2,7 +2,7 @@
     <div class="min-h-screen max-h-screen flex flex-col">
         <header class="w-full whitespace-nowrap">
             <div class="px-5 md:px-10 lg:px-15 xl:px-25 2xl:px-50 py-4 bg-blue-400 flex justify-between overflow-auto">
-                <router-link to="/" class="text-lg md:text-xl font-bold hover:text-blue-800 hover:bg-gray-500 hover:cursor-pointer">
+                <router-link to="/" class="text-lg md:text-xl font-bold hover:text-blue-800 hover:cursor-pointer">
                     [
                     <span class="text-red-800">
                         TASK
@@ -25,7 +25,7 @@
                 </ul>
             </div>
         </header>
-        <div class="flex-grow w-full bg-gray-200 overflow-auto">
+        <div class="px-5 md:px-10 lg:px-15 xl:px-25 2xl:px-50 py-4 flex-grow w-full bg-gray-200 overflow-auto">
             <router-view></router-view>
         </div>
     </div>
