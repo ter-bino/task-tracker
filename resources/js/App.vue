cf0de1f43ded84c1f172e0b185ac64527bd579fb
Teral <altero31@gmail.com>
2023-06-28 18:34:16 +0800

    Cleaned frontend template

---
+++ resources/js/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Task Tracker!
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<script>
+export default {
+    components: {
+        name: 'App'
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
