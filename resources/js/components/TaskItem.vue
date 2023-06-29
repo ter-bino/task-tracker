8fbce4d20806a50303a4f0669609a9cafcb05e63
Teral <altero31@gmail.com>
2023-06-29 12:57:22 +0800

    shortening of descriptions on TaskItem

--- resources/js/components/TaskItem.vue
+++ resources/js/components/TaskItem.vue
@@ -7,7 +7,7 @@
                 <span v-else-if="isImportant" class="text-white px-2 bg-red-500 rounded-full">!</span>
             </h1>
             <p class="text-sm">{{ formattedDeadline }}</p>
-            <p>{{ description }}</p>
+            <p>{{ shortenedDescription }}</p>
         </div>
         <div class="flex flex-col md:flex-row">
             <router-link :to="editLink" class="px-2 my-auto md:mx-2 md:w-32 text-center bg-yellow-500 rounded-lg text-white">EDIT</router-link>
@@ -36,6 +36,14 @@
 
     const editLink = computed(() => {
         return `/tasks/${props.id.toString()}/edit`;
+    })
+    
+    const shortenedDescription = computed(() => {
+        if(props.description.length > 40) {
+            return props.description.slice(0, 40) + "...";
+        } else {
+            return props.description;
+        }
     })
 
     const needsAttention = computed(() => {
