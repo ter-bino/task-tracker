d48078931a26af8007dc4aaf91f6b9b1bfed814d
Teral <altero31@gmail.com>
2023-06-29 11:49:03 +0800

    TaskForm + fix on task list data fetching

--- resources/js/components/TaskForm.vue
+++ resources/js/components/TaskForm.vue
@@ -1,10 +1,93 @@
 <template>
-    <div>
-        Task form place holder
+    <div class="bg-white shadow-md rounded-lg p-4">
+        <div class="border-b-4 border-red-200 text-center font-bold text-gray-500">
+            <h1 v-if="isNewTask">NEW TASK</h1>
+            <h1 v-else>Edit Task</h1>
+        </div>
+        <form @submit.prevent="submitForm" class="flex flex-col p-4">
+            <div class="flex flex-col mb-5">
+                <label for="name" class="w-full text-gray-500 ">Title:</label>
+                <input type="text" id="name" v-model="task.title" class="border-b flex-grow border-gray-400 focus:outline-none text-gray-700" required />
+            </div>
+            <div class="flex flex-col mb-5">
+                <label for="description" class="w-full text-gray-500">Description:</label>
+                <textarea id="description" v-model="task.description" class="border flex-grow border-gray-400 focus:outline-none text-gray-700" required></textarea>
+            </div>
+            <div class="flex flex-col mb-5">
+                <label for="deadline" class="w-full text-gray-500">Deadline:</label>
+                <input type="date" id="deadline" v-model="task.deadline"  class="border flex-grow border-gray-400 focus:outline-none text-gray-700" required />
+            </div>
+            <div class="flex flex-row mb-5 justify-around">
+                <div>
+                    <label for="important" class="text-gray-500">Important? </label>
+                    <input type="checkbox" id="important" v-model="task.isImportant" class="border border-gray-400 focus:outline-none text-gray-700" />
+                </div>
+                <div>
+                    <label for="completed" class="text-gray-500">Completed? </label>
+                    <input type="checkbox" id="completed" v-model="task.isCompleted" class="border border-gray-400 focus:outline-none text-gray-700" />
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <button v-if="isNewTask" type="submit" class="p-1 border border-green-200">Add Task</button>
+                <button v-else type="submit" class="p-1 border border-green-200">Update Task</button>
+            </div>
+        </form>
     </div>
 </template>
 
-<script setup>
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            task: {
+                title: '',
+                description: '',
+                deadline: Date.undefined,
+                isImportant: false,
+                isCompleted: false
+            }
+        }
+    },
+    computed: {
+        isNewTask() {
+            return !this.$route.path.includes('edit');
+        }
+    },
+    async created() {
+        if(!this.isNewTask) {
+            const response = await axios.get(`/api/tasks/${this.$route.params.id}`);
+            this.task = {...response.data,
+                            isCompleted:response.data.isCompleted===1? true: false,
+                            isImportant: response.data.isImportant===1? true: false
+                        };
+        }
+    },
+    methods: {
+        async submitForm() {
+            try {
+                if(this.isNewTask) {
+                    await axios.post('/api/tasks/', this.task);
+                } else {
+                    await axios.put(`/api/tasks/${this.$route.params.id}`, this.task);
+                }
+                this.clearForm();
+            } catch (error) {
+                alert('Failed to submit form')
+            }
+        },
+        clearForm() {
+            this.task = {
+                title: '',
+                description: '',
+                deadline: '',
+                isImportant: false,
+                isCompleted: false
+            }
+        }
+    }
+}
 
 </script>
 
--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -1,13 +1,55 @@
 <template>
     <div>
-        TaskList Placeholder
+      <div v-for="task in tasks" :key="task.id" class="mb-3">
+        <TaskItem :id="task.id" :title="task.title" :description="task.description" :deadline="new Date(task.deadline)" :isImportant="task.isImportant===1? true: false" :isCompleted="task.isCompleted===1? true: false"/>
+      </div>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  import TaskItem from './TaskItem.vue';
+  import axios from 'axios';
+  import { ref } from 'vue';
+  
+  const tasks = ref([]);
+  
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get('/api/tasks');
+      tasks.value = response.data;
+    } catch (error) {
+      alert("Unable to fetch tasks.");
+    }
+  };
+  
+  fetchTasks();
+  </script>
+  
+  <script>
+  export default {
+    name: 'TaskList',
+    components: {
+      TaskItem,
+    },
+    data() {
+      return {
+        tasks: [],
+      };
+    },
+    methods: {
+      async deleteTask(id) {
+        try {
+          await axios.delete(`api/tasks/${id}`);
+          this.tasks = this.tasks.filter((task) => task.id !== id);
+        } catch (error) {
+          alert('Failed to delete task!');
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  
+  </style>
+  
